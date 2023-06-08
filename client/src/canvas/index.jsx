a797401807b014895c5732f1d85691f6a871a491
AbhishekBhure <abhiab7411@gmail.com>
2023-06-08 10:29:19 -0700

    done shirt cameraRig and backdrop

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -2,19 +2,24 @@ import { Canvas } from "@react-three/fiber";
 import { Environment, Center } from "@react-three/drei";
 import Shirt from "./Shirt";
 import Backdrop from "./Backdrop";
-import CamerRig from "./CameraRig";
+import CameraRig from "./CameraRig";
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
-      <CamerRig>
+      <CameraRig>
         <Backdrop />
         <Center>
           <Shirt />
         </Center>
-      </CamerRig>
+      </CameraRig>
     </Canvas>
   );
 };
